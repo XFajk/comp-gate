d2ae3e688342d0188d4d16d340dd73ca0c9b1e1b
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2025-11-10 15:32:25 +0100

    Initialize Rust project with configuration files, dependencies, and basic structure

---
+++ src/helper/device_managment.rs
@@ -0,0 +1,10 @@
+/// This file holds the functions related to device management
+/// such as listing connected devices, ejecting devices, etc.
+
+struct Device;
+
+struct DeviceManager {
+    devices: Vec<Device>,
+}
+
+
